78a8be559043dd380493794eec8c922a004e77bf
SimonaSriubaite <simona.sriubaite@gmail.com>
2020-12-12 22:59:27 +0200

    register and login created

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const middleware = require("./middleware");
 
 const port = process.env.SERVER_PORT || 3000;
 
@@ -26,6 +29,102 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("The Back-End is working");
+});
+
+app.post("/register", middleware.validateUserData, (req, res) => {
+  const username = req.body.username.toLowerCase();
+  con.query(
+    `SELECT * FROM users WHERE username = ${mysql.escape(username)}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ msg: "Internal server error checking username validity" });
+      } else if (result.length !== 0) {
+        return res.status(400).json({ msg: "This username already exists" });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            console.log(err);
+            return res.status(400).json({
+              msg: "Internal server error hashing user details",
+            });
+          } else {
+            con.query(
+              `INSERT INTO users (username, password) VALUES (${mysql.escape(
+                username
+              )}, ${mysql.escape(hash)})`,
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                  return res.status(400).json({
+                    msg: "Internal server error saving user details",
+                  });
+                } else {
+                  return res.status(201).json({
+                    msg: "User has been successfully registered",
+                  });
+                }
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", middleware.validateUserData, (req, res) => {
+  const username = req.body.username.toLowerCase();
+  con.query(
+    `SELECT * FROM users WHERE username = ${mysql.escape(username)}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ msg: "Internal server error gathering user details" });
+      } else if (result.length !== 1) {
+        return res.status(400).json({
+          msg: "The provided details are incorrect or the user does not exist",
+        });
+      } else {
+        bcrypt.compare(
+          req.body.password,
+          result[0].password,
+          (bErr, bResult) => {
+            if (bErr || !bResult) {
+              return res.status(400).json({
+                msg:
+                  "The provided details are incorrect or the user does not exist",
+              });
+            } else if (bResult) {
+              const token = jwt.sign(
+                {
+                  userId: result[0].id,
+                  username: result[0].username,
+                },
+                process.env.SECRET_KEY,
+                {
+                  expiresIn: "7d",
+                }
+              );
+
+              return res.status(200).json({
+                msg: "Logged In",
+                token,
+                userData: {
+                  userId: result[0].id,
+                  username: result[0].username,
+                },
+              });
+            }
+          }
+        );
+      }
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
